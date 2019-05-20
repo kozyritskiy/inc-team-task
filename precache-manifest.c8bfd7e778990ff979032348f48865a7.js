147698d965e8877a8ee167bb3955068dd6d01386
Maxim Kozyritskiy <maxim.kozyricki@gmail.com>
2019-05-20 17:45:25 +0300

    Updates

---
+++ precache-manifest.c8bfd7e778990ff979032348f48865a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8293668096dbccb5937715e09e4746",
+    "url": "/website/static/media/logo.ca829366.png"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "f7e87d73f9fd7bcf9095",
+    "url": "/website/static/js/main.ba79f81d.chunk.js"
+  },
+  {
+    "revision": "071faa982ef254c36d3c",
+    "url": "/website/static/js/2.93a27f6f.chunk.js"
+  },
+  {
+    "revision": "f7e87d73f9fd7bcf9095",
+    "url": "/website/static/css/main.016a6a95.chunk.css"
+  },
+  {
+    "revision": "16c811196aefc2c711eb5df7a13157ea",
+    "url": "/website/index.html"
+  }
+];
